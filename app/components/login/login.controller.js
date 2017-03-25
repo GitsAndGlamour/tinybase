d77146998f11a800e6a67408d30088ce9536284f
Omie <omie_r_walls@homedepot.com>
2017-03-24 23:25:11 -0400

    Login UI finished

--- app/components/login/login.controller.js
+++ app/components/login/login.controller.js
@@ -4,19 +4,44 @@ angular.module('app')
 LoginController.$inject = ['tab', '$scope', '$mdDialog'];
 
 function LoginController(tab, $scope, $mdDialog) {
+  $scope.selectedIndex = tab === 'signup' ? 0 : 1;
   $scope.$onInit = $onInit;
   $scope.cancel = cancel;
-  $scope.selectedIndex = tab === 'signup' ? 0 : 1;
+  $scope.signUp= signUp;
   $scope.login = login;
+  $scope.facebookSignUp = facebookSignUp;
+  $scope.googleSignUp = googleSignUp;
+  $scope.facebookLogin = facebookLogin;
+  $scope.googleLogin = googleLogin;
 
   function $onInit() {
   }
 
-  function cancel() {
-    $mdDialog.hide();
+  function signUp() {
+    console.log('login');
+  }
+
+  function facebookSignUp() {
+    console.log('login');
+  }
+
+  function googleSignUp() {
+    console.log('login');
   }
 
   function login() {
     console.log('login');
+  }
+
+  function facebookLogin() {
+    console.log('login');
+  }
+
+  function googleLogin() {
+    console.log('login');
+  }
+
+  function cancel() {
+    $mdDialog.hide();
   }
 }
