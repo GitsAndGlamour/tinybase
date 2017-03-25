009720db9b81210bea475544b0ddf81363eaebac
Omie <omie_r_walls@homedepot.com>
2017-03-24 22:29:43 -0400

    Start of signup/login screen

--- app/components/login/login.controller.js
+++ app/components/login/login.controller.js
@@ -7,11 +7,16 @@ function LoginController(tab, $scope, $mdDialog) {
   $scope.$onInit = $onInit;
   $scope.cancel = cancel;
   $scope.selectedIndex = tab === 'signup' ? 0 : 1;
+  $scope.login = login;
 
   function $onInit() {
   }
 
   function cancel() {
     $mdDialog.hide();
+  }
+
+  function login() {
+    console.log('login');
   }
 }
