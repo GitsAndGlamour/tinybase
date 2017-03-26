20a8309f2f482dc40b2437ad130a362ff73708fb
Omie <omie_r_walls@homedepot.com>
2017-03-25 22:12:27 -0400

    Added Facebook authorization

--- app/components/login/login.controller.js
+++ app/components/login/login.controller.js
@@ -10,6 +10,7 @@ function LoginController(tab, $scope, $mdDialog,
                          FirebaseService) {
   $scope.selectedIndex = tab === 'signup' ? 0 : 1;
   $scope.$onInit = $onInit();
+  $scope.provider = new firebase.auth.FacebookAuthProvider();
   $scope.cancel = cancel;
   $scope.signUp = signUp;
   $scope.facebookSignUp = facebookSignUp;
@@ -23,12 +24,11 @@ function LoginController(tab, $scope, $mdDialog,
 
   function signUp() {
     FirebaseService.signUpViaEmail($scope.user.email, $scope.user.password);
-    var authType = 'e-mail';
-    getUserData(authType);
+    getUserData();
   }
 
   function facebookSignUp() {
-    console.log('facebookSignUp');
+    facebookLogin();
   }
 
   function googleSignUp() {
@@ -37,12 +37,11 @@ function LoginController(tab, $scope, $mdDialog,
 
   function login() {
     FirebaseService.signInViaEmail($scope.user.email, $scope.user.password);
-    var authType = 'e-mail';
-    getUserData(authType);
+    getUserData();
   }
 
   function facebookLogin() {
-    console.log('facebookLogin');
+    FirebaseService.signInViaFacebook($scope.provider);
   }
 
   function googleLogin() {
@@ -53,8 +52,8 @@ function LoginController(tab, $scope, $mdDialog,
     $mdDialog.hide();
   }
 
-  function getUserData(authType) {
-    FirebaseService.getCurrentUser(authType);
+  function getUserData() {
+    FirebaseService.getCurrentUser();
     $mdDialog.hide();
   }
 }
